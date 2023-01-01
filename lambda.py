31c86d08e7985744bf89c9d9d507b353f0f79b7e
Mehdi TAZI <2340163+MehdiTAZI@users.noreply.github.com>
2023-01-01 20:35:23 +0100

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,68 @@
+import json
+import openai
+import random
+from alexa import AlexaSkill
+
+#put your open api api key here, please check the pricing model
+openai.api_key = "PUT_YOUR_OPENAI_KEY_HERE"
+
+class QuoteSkill(AlexaSkill):
+    def __init__(self):
+        AlexaSkill.__init__(self)
+
+    def on_launch(self, launch_request, session):
+        return get_welcome_message()
+
+    def on_intent(self, intent_request, session):
+        intent = intent_request['intent']
+        intent_name = intent_request['intent']['name']
+
+        if intent_name == "QuoteIntent":
+            return handle_quote_intent(intent, session)
+        else:
+            raise ValueError("Invalid intent")
+
+    def get_welcome_message():
+        speech_output = "Welcome to random quotes"
+        return build_response(create_alexa_response(speech_output))
+
+    # Generate a quote using chatbot engine
+    def handle_quote_intent(intent, session):
+        response = openai.Completion.create(
+            # you can also use  GPT-3 text-davinci-003 engine
+            engine="chatbot",
+            prompt="Give one random quote with it author and explain it in few words.",
+            temperature=0.5,
+            max_tokens=random.randint(150, 1000),
+            top_p=1
+        )
+        quote = response['choices'][0]['text']
+        speech_output = f"Today's quote is : {quote}"
+        
+        return build_response(create_alexa_response(speech_output))
+    
+    def create_alexa_response(output):
+        return {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': output
+            },
+            'reprompt': {
+                'outputSpeech': {
+                    'type': 'PlainText',
+                    'text': None
+                }
+            },
+            'shouldEndSession': True
+        }
+    
+    def build_response(alexa_response):
+        return {
+            'version': '1.0',
+            'sessionAttributes': {},
+            'response': alexa_response
+        }
+      
+    def lambda_handler(event, context):
+      skill = QuoteSkill()
+      return skill.route_request(event)
